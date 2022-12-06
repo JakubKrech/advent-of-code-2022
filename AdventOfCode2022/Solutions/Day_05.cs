5e76cd4ddb12678f2b5a39739a4a05fe337a643e
Jakub Krech <kuba.kre@o2.pl>
2022-12-06 01:31:13 +0000

    Day_05 solution

--- AdventOfCode2022/Solutions/Day_05.cs
+++ AdventOfCode2022/Solutions/Day_05.cs
@@ -1,4 +1,7 @@
-﻿using AdventOfCode2022.Tools;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+using AdventOfCode2022.Tools;
 
 namespace AdventOfCode2022.Solutions
 {
@@ -6,26 +9,109 @@ namespace AdventOfCode2022.Solutions
     {
         public static void Solve()
         {
-            var input = InputData.ReadTestInput("Day_05.txt");
-            // var input = InputData.ReadInput("Day_05.txt");
+            // var input = InputData.ReadTestInput("Day_05.txt");
+            var input = InputData.ReadInput("Day_05.txt");
 
             Console.WriteLine("=== Day 05 ===");
-            Console.WriteLine($"Part 1: {Part1(input)}");
-            Console.WriteLine($"Part 2: {Part2(input)}");
+            Console.WriteLine($"Part 1: {Solution(input, moveMultipleAtOnce: false)}");
+            Console.WriteLine($"Part 2: {Solution(input, moveMultipleAtOnce: true)}");
         }
 
-        private static int Part1(IEnumerable<string> input)
+        private static string Solution(IEnumerable<string> input, bool moveMultipleAtOnce)
         {
+            List<Stack<string>> stacks = new();
+            
+            List<string> stackInputLines = new();
+            List<string> movesInputLines = new();
+
+            bool emptyLineReached = false;
+
+            // Split input data between initial stack data and crates movement data
+            foreach(string line in input)
+            {
+                if(line == "")
+                {
+                    emptyLineReached = true;
+                    continue;
+                }
+
+                if (!emptyLineReached) stackInputLines.Add(line);
+                else movesInputLines.Add(line);
+            }
+
+            // Initialize stacks
+            var stackNumbers = stackInputLines.Last<string>().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int numberOfStacks = int.Parse(stackNumbers.Last());
+
+            for (int i = 0; i < numberOfStacks; i++)
+            {
+                stacks.Add(new Stack<string>());
+            }
+
+            string regexPattern = @"(\[[A-Z]\]|[ ]{3}).?";
+            Regex regex = new(regexPattern);
+
+            for (int i = stackInputLines.Count - 2; i >= 0; i--)
+            {
+                MatchCollection matches = regex.Matches(stackInputLines[i]);
+                
+                for(int j = 0; j < numberOfStacks; j++)
+                {
+                    if(!string.IsNullOrWhiteSpace(matches[j].Value))
+                    {
+                        stacks[j].Push(matches[j].Value.Substring(1,1));
+                    }
+                }
+            }
+
+            // Move crates around
+            regexPattern = "move (\\d+) from (\\d+) to (\\d+)";
+            Regex regex2 = new(regexPattern);
+
+            foreach(var moveCommand in movesInputLines)
+            {
+                Match match = regex2.Match(moveCommand);
+                int howMany = int.Parse(match.Groups[1].Value);
+                int from = int.Parse(match.Groups[2].Value);
+                int to = int.Parse(match.Groups[3].Value);
+                string value;
+
+                if (!moveMultipleAtOnce)
+                {
+                    for (int i = 0; i < howMany; i++)
+                    {
+                        value = stacks[from - 1].Pop();
+                        stacks[to - 1].Push(value);
+                    }
+                }
+                else
+                {
+                    Stack<string> intermediateStorage = new();
+
+                    for (int i = 0; i < howMany; i++)
+                    {
+                        value = stacks[from - 1].Pop();
+                        intermediateStorage.Push(value);
+                    }
+
+                    for (int i = 0; i < howMany; i++)
+                    {
+                        value = intermediateStorage.Pop();
+                        stacks[to - 1].Push(value);
+                    }
+                }
+            }
 
 
-            return -1;
-        }
+            // Print top elements
+            StringBuilder sb = new();
 
-        private static int Part2(IEnumerable<string> input)
-        {
+            foreach (var stack in stacks)
+            {
+                sb.Append(stack.Peek());
+            }
 
-
-            return -1;
+            return sb.ToString();
         }
     }
 }
