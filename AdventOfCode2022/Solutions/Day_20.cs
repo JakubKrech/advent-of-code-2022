211a96d1a403c0f59ac2d45422d495acd1e6190f
Jakub <kuba.kre@o2.pl>
2022-12-20 22:56:05 +0000

    Day_20

---
+++ AdventOfCode2022/Solutions/Day_20.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2022.Tools;
+
+namespace AdventOfCode2022.Solutions
+{
+    internal static class Day_20
+    {
+        public static void Solve()
+        {
+            AoCTools<int>.RunMeasureTimeAndLog(Part1, Part2, dayId: "20", testInput: false);
+        }
+
+        private static int Part1(IEnumerable<string> input)
+        {
+
+
+            return -1;
+        }
+
+        private static int Part2(IEnumerable<string> input)
+        {
+
+
+            return -1;
+        }
+    }
+}
